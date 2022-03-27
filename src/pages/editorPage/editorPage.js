5b6e35a394347590a7563313db62a956fddc76cc
Vishwash Bajpai <vishwash.bajpai87@gmail.com>
2022-03-27 22:52:49 +0530

    adding socket connection

--- src/pages/editorPage/editorPage.js
+++ src/pages/editorPage/editorPage.js
@@ -1,7 +1,66 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react';
+import toast from 'react-hot-toast';
+import ACTIONS from '../../Actions';
 import Client from '../../components/client';
 import Editor from '../../components/editor';
+import { initSocket } from '../../socket';
+import {
+    useLocation,
+    useNavigate,
+    Navigate,
+    useParams,
+} from 'react-router-dom';
 export default function EditorPage() {
+    const socketRef = useRef(null);
+    const codeRef = useRef(null);
+    const location = useLocation();
+    const { roomId } = useParams();
+    const reactNavigator = useNavigate();
+    const [clients, setClients] = useState([]);
+
+    useEffect(() => {
+        const init = async () => {
+            socketRef.current = await initSocket();
+
+            socketRef.current.on('connect_error', (err) => handleErrors(err));
+            socketRef.current.on('connect_failed', (err) => handleErrors(err));
+
+            function handleErrors(e) {
+                console.log('socket error', e);
+                toast.error('Socket connection failed, try again later.');
+                reactNavigator('/');
+            } 
+
+                        socketRef.current.emit(ACTIONS.JOIN, {
+                roomId,
+                username: location.state?.username,
+            });
+
+            // Listening for joined event
+            socketRef.current.on(
+                ACTIONS.JOINED,
+                ({ clients, username, socketId }) => {
+                    if (username !== location.state?.username) {
+                        toast.success(`${username} joined the room.`);
+                        console.log(`${username} joined`);
+                    }
+                    setClients(clients);
+                    // socketRef.current.emit(ACTIONS.SYNC_CODE, {
+                    //     code: codeRef.current,
+                    //     socketId,
+                    // });
+                }
+            );
+        }
+        init();
+    }, []);
+
+
+    if (!location.state) {
+        return <Navigate to="/" />;
+    }
+
+
   return (
     <div className='mainWrap'>
       <div className='aside'>
@@ -11,7 +70,12 @@ export default function EditorPage() {
           </div>
           <h3>Connected</h3>
           <div className='clientsList'>
-            <Client/>
+                        {clients.map((client) => (
+                            <Client
+                                key={client.socketId}
+                                username={client.username}
+                            />
+                        ))}
           </div>
         </div>
         <button className='btn copyBtn'>Copy Room Id</button>
